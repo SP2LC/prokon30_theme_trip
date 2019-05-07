be9d0359702e640504e8092c9dd5f148ec856678
Futa Maeda <s16544@salesio-sp.ac.jp>
2019-05-07 23:04:31 +0900

    MaedaGaMade
    add したい

---
+++ trip trial by前田/trip trial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trip trial
+//
+//  Created by sp2lc on 2019/04/27.
+//  Copyright © 2019 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
