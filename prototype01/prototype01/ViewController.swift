c843a8af045110db62ff210c55ab332d992f01f3
Futa Maeda <s16544@salesio-sp.ac.jp>
2019-05-12 01:03:56 +0900

    update: united
    メニューと履歴とマップをくっつけたやつです。
    マップのところは簡単に作ったやつなので、作りが微妙です。
    また、実機テストのときにマップをタップすると落ちました。

---
+++ prototype01/prototype01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prototype01
+//
+//  Created by sp2lc on 2019/05/11.
+//  Copyright © 2019 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
